9950a563ed58733d3e11f0789a4cb360c13a4276
Deathanni <anni3therockstar@gmail.com>
2025-09-07 18:11:49 +0530

    Lecture 4 array question

---
+++ Lecture_4/company_question.js
@@ -0,0 +1,15 @@
+console.log("Here we will solve a company question");
+
+let arr=["Google","Microsoft","Uber","JP Morgan","Myntra","Flipkart"];
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+arr.splice(1,1,"Ola");
+console.log(arr);
+
+arr.push("Amazon");
+console.log(arr)
+
+
