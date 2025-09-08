6781090e1ea5eb68523b8b847ebcfd3f801057c7
Deathanni <anni3therockstar@gmail.com>
2025-09-08 23:40:23 +0530

    Lecture 3 for in loop

---
+++ Lecture_3/for_in_loop.js
@@ -0,0 +1,4 @@
+let arr=["Mango","Apple","Banana","Cherry","Gauva","Chickoo","Strawberry","SweetCorn","Pears","Watermelon","Rasberry"];
+for(let loop in arr){
+    console.log(`${loop} is ${arr[loop]}`);
+}
